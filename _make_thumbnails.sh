1f5867a4a446177ef0bf03d2cc5bcab74cedeb34
Cheng-Yu (Benjamin) Chiang <cchiang@ucsd.edu>
2024-11-19 03:24:01 -0500

    add content

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,7 @@
-for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
-done
+# for file in images/*.{png}; do
+#     echo "Processing $file"
+#     [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+# done
+
+
+# convert images/mia.png -thumbnail 160x160^ -gravity center -extent 160x160 output.png
